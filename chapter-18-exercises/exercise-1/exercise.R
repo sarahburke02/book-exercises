8aaefecab4d3b51539d91592feae8f6199d0295f
SARAHBURKE <sburke2@uw.edu>
2020-02-14 16:56:57 -0800

    week's exercises

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -2,15 +2,21 @@
 library("httr")
 library("jsonlite")
 
+nyt_key #in new apikey file
+
 # Be sure and check the README.md for complete instructions!
 
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
 
+source("ch18ex_apikey.R") #go to that file, select all, andhit control enter
+
+query_params <- list("api-key" = nyt_key)
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
 
+movie_name <- "Parasite"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -21,18 +27,34 @@ library("jsonlite")
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
 
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+endpoint <- "reviews/search.json"
+
+uri <- paste0(base_uri, endpoint)
+
+query_params_list <- list("api-key" = nyt_key, "movie_name" = movie_name)
+
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
 
+response <- GET(uri, query = query_params_list)
+print(response)
+body <- content(response, "text")
+data <- fromJSON(body)
 
 # What kind of data structure did this produce? A data frame? A list?
+
+is.data.frame(data) #FALSE
+is.list(data) #TRUE
 
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
 
+names(data)
+str(data)
 
 # Flatten the movie reviews content into a data structure called `reviews`
 
